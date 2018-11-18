1e89af2dc51a038c51aeacc5d87eb5468ca64a70
JarekZ92 <z.jarek92@gmail.com>
2018-11-18 23:24:47 +0100

    added state and tasks

--- src/dragAndDrop.js
+++ src/dragAndDrop.js
@@ -1,12 +1,30 @@
-import React, {Component} from 'react'
-import './App'
+import React, { Component } from "react";
+import "./App.css";
 
 export default class dragAndDrop extends Component {
-    render(){
-        return (
-            <div className="main_container">
-            
-            </div>
-        )
-    }
+  state = {
+    tasks: [
+      { name: "Task1", category: "wip", color: "yellow" },
+      { name: "Task2", category: "wip", color: "pink" },
+      { name: "Task3", category: "done", color: "skyblue" }
+    ]
+  };
+
+  render() {
+    var tasks = {
+      wip: [],
+      done: []
+    };
+
+    this.state.tasks.forEach(t => {
+      tasks[t.category].push(
+        <div
+          key={t.name}
+          className="draggable"
+          style={{ backgroundColor: t.color }}
+        />
+      );
+    });
+    return <div className="main_container">Drag and Drop</div>;
+  }
 }
