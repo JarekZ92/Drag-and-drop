025a6d9da0f76b2b4cb13fdfd8ddcb0a78a5cbd4
JarekZ92 <z.jarek92@gmail.com>
2018-11-18 22:22:42 +0100

    added some styles into my squares and prepared render function

---
+++ src/dragAndDrop.js
@@ -0,0 +1,11 @@
+import React {Component} from 'react'
+
+export default class dragAndDrop extends Component {
+    render(){
+        return (
+            <div className="main_container">
+            
+            </div>
+        )
+    }
+}
